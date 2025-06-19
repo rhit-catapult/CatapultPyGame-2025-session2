7269c77d505067aaa27dcd84e9dc7a3ec7923c3e
Kyle Wilson <kyle.lyndon.wilson@gmail.com>
2025-06-19 12:36:58 -0400

    Final version

--- 02-DogBark/DogBark.py
+++ 02-DogBark/DogBark.py
@@ -12,8 +12,6 @@ def main():
     # initialize the pygame module
     pygame.init()
 
-    # a change
-    
     # prepare the window (screen)
     screen = pygame.display.set_mode((IMAGE_SIZE, IMAGE_SIZE + TEXT_HEIGHT))
     pygame.display.set_caption("Text, Sound, and an Image")
@@ -22,28 +20,31 @@ def main():
     # DONE 1: Create an image with the 2dogs.JPG image
     image1 = pygame.image.load("2dogs.JPG")
 
-    # TODO 3: Scale the image to be the size (IMAGE_SIZE, IMAGE_SIZE)
+    # DONE 3: Scale the image to be the size (IMAGE_SIZE, IMAGE_SIZE)
     image1 = pygame.transform.scale(image1, (IMAGE_SIZE, IMAGE_SIZE))
 
     # Prepare the text caption(s)
-    # TODO 4: Create a font object with a size 28 font.
+    # DONE 4: Create a font object with a size 28 font.
     font1 = pygame.font.SysFont("comicsans", 28)
     font2 = pygame.font.SysFont("comicsans", 48)
 
 
-    # TODO 5: Render the text "Two Dogs" using the font object (it's like MAKING an image).
+    # DONE 5: Render the text "Two Dogs" using the font object (it's like MAKING an image).
     caption1 = font1.render("Two Dogs", True, BLACK)
     caption2 = font2.render("STAHP IT", True, WHITE)
 
 
     # Prepare the music
-    # TODO 8: Create a Sound object from the "bark.wav" file.
+    # DONE 8: Create a Sound object from the "bark.wav" file.
+    bark_sound = pygame.mixer.Sound("bark.wav")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            # TODO 9: Play the music (bark) if there's a mouse click.
+            # DONE 9: Play the music (bark) if there's a mouse click.
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                bark_sound.play()
 
         # Clear the screen and set the screen background
         screen.fill(WHITE)
